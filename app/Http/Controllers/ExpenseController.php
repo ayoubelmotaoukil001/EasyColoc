ea174d840276bb7eea2f145e9a085f72ae0bd34c
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-02-26 11:00:39 +0000

    feat(ExpenseController): add store and create methods for expenses

---
+++ app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreExpenseRequest ;
+use App\Models\Expense ;
+use App\Models\Category ;
+
+class ExpenseController extends Controller
+{
+    public function store(Request $request)
+{
+    $user = auth()->user();
+    $colocation = $user->colocations()->wherePivot('left_at', null)->first();
+
+    $validatedData = $request->validate([
+        'title' => 'required|string|max:255',
+        'amount' => 'required|numeric|min:0',
+        'category_id' => 'required|exists:categories,id',
+        'entry_date' => 'required|date',
+    ]);
+
+    \App\Models\Expense::create([
+        'title' => $validatedData['title'],
+        'amount' => $validatedData['amount'],
+        'category_id' => $validatedData['category_id'],
+        'user_id' => $user->id,
+        'colocation_id' => $colocation->id,
+        'entry_date' => $validatedData['entry_date'],
+    ]);
+
+    // هذا السطر يمنع الصفحة البيضاء ويعيدك للرئيسية
+    return redirect()->route('dashboard');
+}
+    public function create()
+    {
+        $categories = Category::all() ;
+        return view('expenses.create' , compact('categories')) ;
+    }
+}
