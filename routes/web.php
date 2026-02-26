77ca1d303c4645a35c8b03610644e91436f3efa4
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-02-26 11:55:19 +0000

    feat(web_routes): define dashboard and expense management routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExpensCOntroller ;
+use Termwind\Components\Raw;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::middleware('auth') ->group( function (){
+    Route::get('/expenses/create',[ExpensCOntroller::class,'create'])->name('expenses.create') ;
+    Route::post('/expenses' ,[ExpensCOntroller::class ,'store'])->name('expenses.store');
+}) ;
+require __DIR__.'/auth.php';
