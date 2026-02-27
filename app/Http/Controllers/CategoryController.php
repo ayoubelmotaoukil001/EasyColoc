49e22e82930e0f9b94de52f0b17ae6f1a2829640
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-02-27 09:32:13 +0000

    add function index in categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category ;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index()     
     {
-        //
+        $categories = Category::all() ;
+        return view('categories.index',compact('categories')) ;
     }
 
     /**
